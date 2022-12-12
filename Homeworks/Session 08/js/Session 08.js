95e52e78b27cb0b167bd8c67addd4ab9faa06324
lethanhpa <lethanhpa1406@gmail.com>
2022-12-12 21:13:58 +0700

    Homeworks

--- Homeworks/Session 08/js/Session 08.js
+++ Homeworks/Session 08/js/Session 08.js
@@ -1,47 +1,46 @@
-function chan_le(params) {
-    let input = prompt('Nhap n: ')
+// function chan_le(params) {
+//     let input = prompt('Nhap n: ')
 
-    if(input % 2 ===0) {
-         console.log(input + ' la so chan')
-    } else {
-         console.log(input + ' la so le')
-    }
-}
-chan_le()
-
-
-function bt1(params) {
-    let input = prompt('Nhap n: ')
-    console.log(input*2 + ' la so gap doi so '+ input)
-}
-bt1()
+//     if(input % 2 ===0) {
+//          console.log(input + ' la so chan')
+//     } else {
+//          console.log(input + ' la so le')
+//     }
+// }
+// chan_le()
 
 
-function bt2(params) {
-    let NUMBER_MIN = prompt('Nhap so thu nhat: ');
-    let b = prompt('Nhap so thu hai: ');
-    let c = prompt('Nhap so thu ba: ');
-    let d = prompt('Nhap so thu tu: ');
+// function bt1(params) {
+//     let input = prompt('Nhap n: ')
+//     console.log(input*2 + ' la so gap doi so '+ input)
+// }
+// bt1()
 
-    if(parseInt(b) < parseInt(NUMBER_MIN)) NUMBER_MIN = b
-    if(c < NUMBER_MIN) NUMBER_MIN = c
-    if(d < NUMBER_MIN) NUMBER_MIN = d
-    console.log(NUMBER_MIN + ' la so nho nhat');
-}
-bt2()
+
+// function bt2(params) {
+//     let NUMBER_MIN = prompt('Nhap so thu nhat: ');
+//     let b = prompt('Nhap so thu hai: ');
+//     let c = prompt('Nhap so thu ba: ');
+//     let d = prompt('Nhap so thu tu: ');
+
+//     if(parseInt(b) < parseInt(NUMBER_MIN)) NUMBER_MIN = b
+//     if(c < NUMBER_MIN) NUMBER_MIN = c
+//     if(d < NUMBER_MIN) NUMBER_MIN = d
+//     console.log(NUMBER_MIN + ' la so nho nhat');
+// }
+// bt2()
 
 
 function bt3(params) {
     let input = prompt('Nhap n: ')
-    let a = (input - 1)/2;
-    console.log(a);
+    console.log(parseInt((input - 1)/2));
 }
 bt3()
 
 
-function bt4(params) {
-    let input = prompt('Nhap so co 2 chu so: ');
-    console.log(parseInt(input/10) + ' la chu so hang chuc');
-    console.log(parseInt(input%10) + ' la chu so hang don vi');
-}
-bt4()
+// function bt4(params) {
+//     let input = prompt('Nhap so co 2 chu so: ');
+//     console.log(parseInt(input/10) + ' la chu so hang chuc');
+//     console.log(parseInt(input%10) + ' la chu so hang don vi');
+// }
+// bt4()
