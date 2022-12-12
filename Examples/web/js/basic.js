a764b215f53edb149252662e07aab9593b66eceb
lethanhpa <lethanhpa1406@gmail.com>
2022-12-12 21:08:29 +0700

    Homeworks

---
+++ Examples/web/js/basic.js
@@ -0,0 +1,13 @@
+let input = prompt('Nhap n: ')
+
+if(input % 2 ===0) {
+    console.log(input + ' la so chan')
+} else {
+    console.log(input + ' la so le')
+}
+
+console.log(`${input} la so ${input % 2 === 0 ? "chan" : "le"}`);
+
+function bt1() {
+    
+}
