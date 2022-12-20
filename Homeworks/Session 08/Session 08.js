b92e3ec3a7b0ee4e5d003894083786f749d19835
lethanhpa <lethanhpa1406@gmail.com>
2022-12-20 23:08:36 +0700

    Homeworks

---
+++ Homeworks/Session 08/Session 08.js
@@ -0,0 +1,53 @@
+// function bt1(params) {
+//   let n = prompt("Nhap day so tu nhien:");
+//   function reverse_num(n) {
+//     n = n.toString();
+//     return n.split("").reverse().join("");
+//   }
+//   function symmetrical_num(n) {
+//     let str = false;
+//     if (reverse_num(n) == n) str = true;
+//     return str;
+//   }
+
+//   let check = symmetrical_num(n);
+
+//   if (check == true) console.log(n + " la day so doi xung");
+//   else console.log(n + " khong phai la day so doi xung");
+// }
+// bt1();
+
+// function bt2(params) {
+//     let arr = new Array(prompt("Nhap:"));
+
+//     for (let i = 0; i < arr.length; i++) {
+//     // Vòng lặp thứ hai áp dụng cho mảng bên trong
+//     for (let j = 0; j < arr[i].length; j++) {
+//         // Truy xuất các phần tử của từng mảng con
+//         console.log(arr[i][j]);
+//     }
+// }
+// }
+// bt2()
+
+// function bt3(params) {
+//   let array = [91, 451, 43, 3, 452, 54];
+//   array.sort(function (a, b) {
+//     return a - b;
+//   });
+//   console.log(array);
+//   let k = prompt("Nhap so lon thu:");
+//   console.log("So lon thu " + k + " la: " + array[array.length - k]);
+// }
+// bt3();
+
+// function bt4(params) {
+//   let n = prompt("Nhap:");
+//   let sum = 0;
+//   for (let i = 0; i = n; i++) {
+//     sum += n % 10;
+//     n = Math.floor(n / 10);
+//   }
+//   console.log(sum);
+// }
+// bt4()
